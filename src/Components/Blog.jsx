1ed26583539501ee836f90354b2ad37d58f3edbe
Vishal Ingle <vishaling208@gmail.com>
2026-08-06 00:43:44 +0530

    Fix Blog imports for Vercel

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,17 +1,11 @@
-
-import Header from "../Components/Header";
-import Navbar from "../Components/Navbar";
-import BlogSection from "../Components/BlogSection";
-import Footer from "../Components/Footer";
+import BlogSection from "./BlogSection";
 
 function Blog() {
   return (
     <>
       <div className="bg-blue-900 py-16">
         <div className="max-w-7xl mx-auto px-5 text-center text-white">
-          <h1 className="text-4xl font-bold">
-            Our Blogs
-          </h1>
+          <h1 className="text-4xl font-bold">Our Blogs</h1>
 
           <p className="mt-3">
             Latest News, Electrical Safety & Industry Updates
@@ -20,7 +14,7 @@ function Blog() {
       </div>
 
       <BlogSection />
-      </>
+    </>
   );
 }
 
