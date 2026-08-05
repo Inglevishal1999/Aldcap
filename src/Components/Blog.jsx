b73883b3b3aaef87ec1fd5f3c1601e6133e44340
Vishal Ingle <vishaling208@gmail.com>
2026-08-06 00:39:56 +0530

    Fix Blog component imports

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,8 +1,8 @@
 
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
-import BlogSection from "../components/BlogSection";
-import Footer from "../components/Footer";
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import BlogSection from "../Components/BlogSection";
+import Footer from "../Components/Footer";
 
 function Blog() {
   return (
