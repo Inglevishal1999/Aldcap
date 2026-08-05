eb6250010cf5e141829ce3185d518d705decbd26
Vishal Ingle <vishaling208@gmail.com>
2026-08-06 00:29:39 +0530

    Fix App imports and remove duplicate routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,45 +7,27 @@ import Services from "./Components/Services";
 import Careers from "./Components/Career";
 import Blog from "./Components/Blog";
 import Contact from "./Components/Contact";
-import Header from "./components/Header";
+import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
-
-    <Header />
-    <Navbar />
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/blog" element={<Blog />} />
-      {/* <Route path="/consumers" element={<Consumers />} /> */}
-      <Route path="/careers" element={<Careers />} />
-      {/* <Route path="/contact" element={<Contact />} /> */}
-    </Routes>
-    <Footer />
       <Header />
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/about" element={<About />} />
-
         <Route path="/services" element={<Services />} />
-
-        {/* <Route path="/consumers" element={<Consumers />} /> */}
-
         <Route path="/blog" element={<Blog />} />
-
+        {/* <Route path="/consumers" element={<Consumers />} /> */}
         <Route path="/careers" element={<Careers />} />
-
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
+
+      <Footer />
     </>
   );
 }
